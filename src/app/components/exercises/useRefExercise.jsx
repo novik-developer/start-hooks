b9464942ae13f08c4fe9b2d5dc4689d80b4d90b0
Andrey Novik <novik.93@bk.ru>
2023-01-18 13:52:58 +0300

    fixed branch

--- src/app/components/exercises/useRefExercise.jsx
+++ src/app/components/exercises/useRefExercise.jsx
@@ -1,12 +1,6 @@
-import React, { useRef } from "react";
+import React from "react";
 import CollapseWrapper from "../common/collapse";
 const UseRefExercise = () => {
-    const blockRef = useRef();
-    const handleClick = () => {
-        blockRef.current.style.width = "80px";
-        blockRef.current.style.height = "150px";
-        blockRef.current.querySelector("small").innerText = "text";
-    };
     return (
         <CollapseWrapper title="Упражнение">
             <p className="mt-3">
@@ -18,7 +12,6 @@ const UseRefExercise = () => {
                 <li>высота и ширина станут равны 150 и 80 соответственно</li>
             </ul>
             <div
-                ref={blockRef}
                 className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
                 style={{
                     height: 40,
@@ -28,9 +21,6 @@ const UseRefExercise = () => {
             >
                 <small>Блок</small>
             </div>
-            <button className="btn btn-secondary mt-3" onClick={handleClick}>
-                Click
-            </button>
         </CollapseWrapper>
     );
 };
